560c6091a05b46bd27cd8b462d631954990eab36
Art Vardanyan <akvardanyan99@gmail.com>
2024-04-10 17:41:21 -0400

    a

--- frontend/components/Todo.test.js
+++ frontend/components/Todo.test.js
@@ -2,24 +2,34 @@ import React from 'react'
 import { render, waitFor, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
+import Todo from './Todo'
 import server from '../../backend/mock-server'
 import { resetTodos } from '../../backend/helpers'
 
 describe('Todos Component', () => {
+  let user, laundry, dishes, groceries, input
 
   afterEach(() => { server.resetHandlers() })
   beforeAll(() => { server.listen() })
   afterAll(() => { server.close() })
   beforeEach(async () => {
     resetTodos()
+    render(<Todo />) 
+    user = userEvent.setup()
   })
 
-  test('all todos are present', () => {
+  test('all todos are present', async () => {
     // screen.debug()
+    await waitFor(() => {
+      expect(screen.findByText('laundry')).toBeInTheDocument()
+      screen.findByText('dishes')
+      screen.findByText('groceries')
+    }) 
+    await screen.findByText('laundry')
+
 
   })
   test('can do and undo todos', async () => {
-
   })
   test('can delete todos', async () => {
 
